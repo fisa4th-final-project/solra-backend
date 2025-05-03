671a5e65473e001561ec028c652f7708ef3d480c
JaeHee-devSpace <jaehee1658@gmail.com>
2025-05-03 16:30:21 +0900

    feat: 권한 생성 api 추가

--- app/src/main/java/com/fisa/solra/domain/permission/controller/PermissionController.java
+++ app/src/main/java/com/fisa/solra/domain/permission/controller/PermissionController.java
@@ -1,7 +1,12 @@
 package com.fisa.solra.domain.permission.controller;
 
+import com.fisa.solra.domain.permission.dto.PermissionRequestDto;
+import com.fisa.solra.domain.permission.dto.PermissionResponseDto;
 import com.fisa.solra.domain.permission.service.PermissionService;
+import com.fisa.solra.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class PermissionController {
 
     private final PermissionService permissionService;
-
+    @PostMapping
+    public ResponseEntity<ApiResponse<PermissionResponseDto>> createPermission(
+            @RequestBody PermissionRequestDto requestDto) {
+        PermissionResponseDto dto = permissionService.createPermission(requestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(dto, "권한 생성 성공"));
+    }
 }
--- app/src/main/java/com/fisa/solra/domain/permission/repository/PermissionRepository.java
+++ app/src/main/java/com/fisa/solra/domain/permission/repository/PermissionRepository.java
@@ -4,5 +4,6 @@ import com.fisa.solra.domain.permission.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    boolean existsByPermissionName(String permissionName);
 }
 
