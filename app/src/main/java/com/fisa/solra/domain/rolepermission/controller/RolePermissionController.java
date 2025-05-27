71bfb49d4c8381227fa81ff44e356b6afd7f86c9
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-05-27 14:33:52 +0900

    refactor: 역할/권한 API들 권한 적용

--- app/src/main/java/com/fisa/solra/domain/rolepermission/controller/RolePermissionController.java
+++ app/src/main/java/com/fisa/solra/domain/rolepermission/controller/RolePermissionController.java
@@ -8,6 +8,7 @@ import com.fisa.solra.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class RolePermissionController {
 
     //역할에 권한 추가
     @PostMapping
+    @PreAuthorize("@permissionService.checkPermission('ROLE_PERMISSION_ASSIGN')")
     public ResponseEntity<ApiResponse<List<RolePermissionResponseDto>>> assignPermissions(
             @RequestBody RolePermissionRequestDto requestDto) {
         List<RolePermissionResponseDto> dto = rolePermissionService.assignPermission(requestDto);
@@ -31,6 +33,7 @@ public class RolePermissionController {
 
     //특정 역할에 부여된 권한 조회
     @GetMapping("/{roleId}")
+    @PreAuthorize("@permissionService.checkPermission('ROLE_PERMISSION_READ')")
     public ResponseEntity<ApiResponse<List<PermissionResponseDto>>> getRolePermissions(
             @PathVariable Long roleId) {
         List<PermissionResponseDto> list = rolePermissionService.getPermissionsByRole(roleId);
@@ -39,6 +42,7 @@ public class RolePermissionController {
 
     //역할에서 권한 제거
     @DeleteMapping
+    @PreAuthorize("@permissionService.checkPermission('ROLE_PERMISSION_REVOKE')")
     public ResponseEntity<ApiResponse<Void>> removePermission(
             @RequestBody RolePermissionRequestDto requestDto) {
         rolePermissionService.removePermissions(requestDto);
