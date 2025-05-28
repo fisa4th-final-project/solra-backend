acd649b843278f47bcffad6b91d5dbb9023fd102
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-05-28 14:43:33 +0900

    fix: cors https://* 허용

--- app/src/main/java/com/fisa/solra/global/config/SecurityConfig.java
+++ app/src/main/java/com/fisa/solra/global/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해
-                        .allowedOrigins("http://localhost:5173", "http://solra-frontend") // 오리진 명확히 명시
+                        .allowedOriginPatterns("https://*") // 오리진 명확히 명시
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 쿠키 허용
