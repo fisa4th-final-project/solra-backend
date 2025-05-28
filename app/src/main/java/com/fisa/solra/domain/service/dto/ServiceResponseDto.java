b927beb2d389b41f9d8ec45eabc47508af17d341
JaeHee-devSpace <jaehee1658@gmail.com>
2025-05-28 16:20:05 +0900

    refactor: 서비스 다중 포트 생성 및 수정 로직 추가

--- app/src/main/java/com/fisa/solra/domain/service/dto/ServiceResponseDto.java
+++ app/src/main/java/com/fisa/solra/domain/service/dto/ServiceResponseDto.java
@@ -1,13 +1,13 @@
 package com.fisa.solra.domain.service.dto;
 
 import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.ServicePort;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -17,15 +17,37 @@ public class ServiceResponseDto {
     private String type;
     private String clusterIP;
     private Map<String, String> selector;
-    private List<ServicePort> ports;
+    private List<Port> ports;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Port {
+        private String name;
+        private Integer port;
+        private Integer targetPort;
+        private String protocol;
+        private Integer nodePort;
+    }
 
     public static ServiceResponseDto from(Service svc) {
+        var spec = svc.getSpec();
+        List<Port> list = spec.getPorts().stream()
+                .map(p -> Port.builder()
+                        .name(p.getName())
+                        .port(p.getPort())
+                        .targetPort(p.getTargetPort() != null ? p.getTargetPort().getIntVal() : null)
+                        .protocol(p.getProtocol())
+                        .nodePort(p.getNodePort())
+                        .build())
+                .collect(Collectors.toList());
+
         return ServiceResponseDto.builder()
                 .name(svc.getMetadata().getName())
-                .type(svc.getSpec().getType())
-                .clusterIP(svc.getSpec().getClusterIP())
-                .selector(svc.getSpec().getSelector())
-                .ports(svc.getSpec().getPorts())
+                .type(spec.getType())
+                .clusterIP(spec.getClusterIP())
+                .selector(spec.getSelector())
+                .ports(list)
                 .build();
     }
 }
